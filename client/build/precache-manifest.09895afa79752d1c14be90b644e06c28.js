3d4c8269a89d4a62f4f99b0771151193fed7552c
Alex Hansen <Hansen.Alexander.J@Gmail.com>
2019-09-17 09:56:55 -0400

    Fixed axios

---
+++ client/build/precache-manifest.09895afa79752d1c14be90b644e06c28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3c20842bb8d680cc2e17179d2d655e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "051afdf276ab7c459a6e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "60d54c66eb6cf52bb511",
+    "url": "/static/js/2.ae6dacf0.chunk.js"
+  },
+  {
+    "revision": "051afdf276ab7c459a6e",
+    "url": "/static/js/main.59d5b71c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
